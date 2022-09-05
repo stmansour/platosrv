368dd109d020e67ffafde105623d72f7c9879ae4
Steve Mansour <sman@stevemansour.com>
2022-09-05 13:55:10 -0700

    wip

--- webui/sim1/maxex.js
+++ webui/sim1/maxex.js
@@ -7,11 +7,14 @@ class MaxExch extends Influencer {
         super();                // generates the id
         this.ticker = ticker;
         this.records = [];      // place to store exch data used
+        this.dtRecords = null,  // date associated with this.records (which may be empty, that's why we need the date)
         this.predictions = 0;
         this.correctPredictions = 0;
         this.archive = [];
         this.dt = null;         // what date am I currently processing
         this.dataCollected = false;
+        this.responseProcessed = true;
+        this.awaitingServerReply = false;
     }
 
     init() {
@@ -19,23 +22,42 @@ class MaxExch extends Influencer {
     }
 
     go() {
-        if (!this.dataCollected) {
-            if (this.records.length > 0) {
-                let v = this.processRecords();
-                let info = {
-                    dt: this.dt,
-                    low: v[0],
-                    high: v[1],
-                };
+        if (this.dataCollected || this.awaitingServerReply) {
+            return;
+        }
+        if (!this.responseProcessed) {
+            //-------------------------------------
+            // Process the server response now...
+            //-------------------------------------
+            let info = this.getNewInfo();
+            if (this.records.length >= 0) {
+                if (this.records.length > 1) { // we added one record when the result was 0 records.  The expected value is 1440
+                    info.found = true;
+                    let r = this.processRecords();
+                    info.low = r.low;
+                    info.high = r.high;
+                    info.dtLow = r.dtLow;
+                    info.dtHigh = r.dtHigh;
+                } else {
+                    info = this.records[0];
+                }
                 this.archive.push(info);
                 this.records = [];
-                this.dt.setDate(this.dt.getDate() + 1); // move to the next day
-                if (this.dt > app.simulator.dtStop) {
-                    this.dataCollected = true;
-                }
-            } else {
-                this.fetchExchData([this.ticker],this.dt);
             }
+            this.responseProcessed = true;
+            //---------------------------------------
+            // Set up future requests...
+            //---------------------------------------
+            this.dt.setDate(this.dt.getDate() + 1); // move to the next day
+            if (this.dt > app.simulator.dtStop) {
+                this.dataCollected = true;
+            }
+        } else {
+            //----------------------------------------------
+            // We have not collected all the data we need.
+            // Fetch the next bit.
+            //----------------------------------------------
+            this.fetchExchData([this.ticker],this.dt);
         }
     }
 
@@ -47,43 +69,65 @@ class MaxExch extends Influencer {
             tickers: tickers,
             dt: s,
         };
+        let saveDt = s;
         var dat = JSON.stringify(params);
+        this.awaitingServerReply = true;
+        this.responseProcessed = false;
         $.post('http://localhost:8277/v1/exch/', dat, null, "json")
         .done( (data) => {
+            this.awaitingServerReply = false;
             if (data.total == 0) {
                 console.log("NO RECORDS FOUND for " +s);
+                let info = this.getNewInfo();
+                info.dtLow = saveDt;
+                info.dtHigh = saveDt;
+                this.records.push(info);  // save this so we have the date
                 return;
             }
             if (data.status === "success") {
                 console.log("Successfully retrieved " + data.records.length + " records from plato server.");
                 this.records = data.records;
+                this.dtRecords = saveDt;
             } else {
                 console.log("Login service returned unexpected status: " + data.status);
             }
         })
         .fail(function(/*data*/){
+            this.awaitingServerReply = false;
             console.log("Request failed");
         });
     }
 
-    // print out relevant information about this MaxExch object
-    // show(id) {
-    // }
+    getNewInfo() {
+        return {
+            dtLow: null,
+            dtHigh: null,
+            low: 0,
+            high: 0,
+            found: false, // assume nothing found on this date
+        };
+    }
 
     // processRecords returns an array of 2 numbers: the low and the high of the day
     //------------------------------------------------------------------------------
     processRecords() {
-        let high = 0;
-        let low = Infinity;
+        let r = {
+            high: 0,
+            dtHigh: null,
+            low: Infinity,
+            dtLow: null,
+        };
         for (var i = 0; i < this.records.length; i++) {
-            if (this.records[i].Close > high) {
-                high = this.records[i].High;
+            if (this.records[i].Close > r.high) {
+                r.high = this.records[i].High;
+                r.dtHigh = this.records[i].Dt;
             }
-            if (low > this.records[i].Low) {
-                low = this.records[i].Low;
+            if (r.low > this.records[i].Low) {
+                r.low = this.records[i].Low;
+                r.dtLow = this.records[i].Dt;
             }
         }
-        return [low,high];
+        return r;
     }
 
 }
--- webui/sim1/ui.js
+++ webui/sim1/ui.js
@@ -7,16 +7,3 @@ function initUI() {
     setInnerHTML("populationSize",app.populationSize);
     setTDBG("toprow14","red");
 }
-
-function formatDate(dt) {
-    const m = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-    return m[dt.getMonth()] + " " + dt.getDate() + ", " + dt.getFullYear();
-}
-
-// add the slash for readability in the database ticker string
-function formatTicker(s) {
-    if (s.length < 4) {
-        return s;
-    }
-    return s.slice(0, 3) + "/" + s.slice(3);
-}
